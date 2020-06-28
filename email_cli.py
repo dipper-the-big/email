2280b127af9135b5a0107c466c28ae6ebc4d83b9
Zack Carnet <zackcarnet1@gmail.com>
2020-06-29 03:52:55 +0530

    Added prompting vs direct option feature

--- email_cli.py
+++ email_cli.py
@@ -3,20 +3,24 @@ import smtplib
 import click
 from email.message import EmailMessage
 
-
 @click.command()
 @click.argument('to')
-@click.option('-t','--text', prompt=True, required=False)
+@click.option('-t', 't', is_flag=True)
+@click.option('-T','--Text', required=False)
 @click.option('-s','--subject', default='')
-def email(to, text, subject):
+def email(to, t, text, subject):
     EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
     EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+    if t:
+        text = click.prompt('Text')
 
     msg = EmailMessage()
     msg['Subject'] = subject
     msg['FROM'] = EMAIL_ADDRESS
-    msg['TO'] = contacts[to] if (to in contacts.keys()) else to
-    msg.set_content(text)
+    msg['TO'] = to
+    if text:
+        msg.set_content(text)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
