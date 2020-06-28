fd4b63e5657545d1bc051aa8e501880ee813a592
Zack Carnet <zackcarnet1@gmail.com>
2020-06-29 02:59:41 +0530

    Converted to Email message and Added Error Handling

--- email_cli.py
+++ email_cli.py
@@ -1,25 +1,28 @@
 import os
 import smtplib
 import click
+from email.message import EmailMessage
 
-#TODO: add venv
 
 @click.command()
 @click.argument('to')
-@click.option('-t','--text', prompt=True)
+@click.option('-t','--text', prompt=True, required=False)
 @click.option('-s','--subject', default='')
 def email(to, text, subject):
     EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
     EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['FROM'] = EMAIL_ADDRESS
+    msg['TO'] = contacts[to] if (to in contacts.keys()) else to
+    msg.set_content(text)
+
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
-        body = text
-
-        msg = f'Subject: {subject}\n\n{body}'
-
-        smtp.sendmail(EMAIL_ADDRESS, to, msg)
-
-        click.echo('Sent email')
+        try:
+            smtp.send_message(msg)
+        except smtplib.SMTPRecipientsRefused:
+            click.echo(click.style('Invalid Email Address',fg='red'))
+        else:
+            click.echo(click.style('Sent email',fg='green'))
