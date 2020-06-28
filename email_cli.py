6cf88856f24d07977a2eb318b745683e2eef502f
Zack Carnet <zackcarnet1@gmail.com>
2020-06-29 05:26:47 +0530

    Added file transfer

--- email_cli.py
+++ email_cli.py
@@ -5,15 +5,19 @@ from email.message import EmailMessage
 
 @click.command()
 @click.argument('to')
-@click.option('-t', 't', is_flag=True)
-@click.option('-T','--Text', required=False)
+@click.option('-T', 't', is_flag=True)
+@click.option('-t','--text', required=False)
+@click.option('-F', 'f', is_flag=True)
+@click.option('-f', '--file', required=False, multiple=True)
 @click.option('-s','--subject', default='')
-def email(to, t, text, subject):
+def email(to, t, text, f,  file, subject):
     EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
     EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
     if t:
         text = click.prompt('Text')
+    if f:
+        file = click.prompt('File').split(' ')
 
     msg = EmailMessage()
     msg['Subject'] = subject
@@ -21,6 +25,10 @@ def email(to, t, text, subject):
     msg['TO'] = to
     if text:
         msg.set_content(text)
+    if file:
+        for file in file:
+            with click.open_file(file,'rb') as file:
+                msg.add_attachment(file.read(), maintype='application', subtype='octet-stream', filename=file.name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
