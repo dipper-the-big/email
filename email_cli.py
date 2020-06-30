31c25ecdb71eb9752d0328ae629766e46cb2ec14
Zack Carnet <zackcarnet1@gmail.com>
2020-06-30 20:40:20 +0530

    Added Custom Sender funtionality

--- email_cli.py
+++ email_cli.py
@@ -5,14 +5,17 @@ from email.message import EmailMessage
 
 @click.command()
 @click.argument('to', nargs=-1)
+@click.option('-u', '--user', default=os.environ.get('EMAIL_ADDRESS'))
 @click.option('-T', 't', is_flag=True)
 @click.option('-t','--text', required=False)
 @click.option('-F', 'f', is_flag=True)
 @click.option('-f', '--file', required=False, multiple=True)
 @click.option('-s','--subject', default='')
-def email(to, t, text, f,  file, subject):
-    EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
-    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+def email(to, user, t, text, f,  file, subject):
+    if user != os.environ.get('EMAIL_ADDRESS'):
+        EMAIL_PASSWORD = click.prompt('Password')
+    else:
+        EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
     if t:
         text = click.prompt('Text')
@@ -21,7 +24,7 @@ def email(to, t, text, f,  file, subject):
 
     msg = EmailMessage()
     msg['Subject'] = subject
-    msg['FROM'] = EMAIL_ADDRESS
+    msg['FROM'] = user
     msg['TO'] = to
     if text:
         msg.set_content(text)
@@ -31,7 +34,12 @@ def email(to, t, text, f,  file, subject):
                 msg.add_attachment(file.read(), maintype='application', subtype='octet-stream', filename=file.name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        try:
+            smtp.login(user, EMAIL_PASSWORD)
+        except smtplib.SMTPAuthenticationError:
+            click.echo(click.style('Authentication Error', fg='red'))
+            exit()
+
         try:
             smtp.send_message(msg)
         except smtplib.SMTPRecipientsRefused:
