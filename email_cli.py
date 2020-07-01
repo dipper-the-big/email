3bdd61d31a935c321cc19a9664c192ff827a71c5
Zack Carnet <zackcarnet1@gmail.com>
2020-07-02 00:22:42 +0530

    Added Config Command

--- email_cli.py
+++ email_cli.py
@@ -7,6 +7,11 @@ from email.message import EmailMessage
 config = configparser.ConfigParser()
 config.read('email_cli.cfg')
 aliases = config['Aliases']
+settings = config['Settings']
+
+server = settings.get('server', 'smtp.gmail.com 465').split(' ')
+address_at = settings.get('address_at', 'EMAIL_ADDRESS')
+password_at = settings.get('password_at', 'EMAIL_PASSWORD')
 
 @click.group()
 def email():
@@ -14,7 +19,7 @@ def email():
 
 @email.command()
 @click.argument('to', nargs=-1)
-@click.option('-u', '--user', default=os.environ.get('EMAIL_ADDRESS'))
+@click.option('-u', '--user', default=os.environ.get(address_at))
 @click.option('-T', 't', is_flag=True)
 @click.option('-t','--text', required=False)
 @click.option('-F', 'f', is_flag=True)
@@ -24,10 +29,11 @@ def send(to, user, t, text, f,  file, subject):
     to = resolve(list(to))
     user = aliases.get(user, user)
 
-    if user != os.environ.get('EMAIL_ADDRESS'):
+    if user != os.environ.get(address_at):
         EMAIL_PASSWORD = click.prompt('Password')
     else:
-        EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+        print(password_at)
+        EMAIL_PASSWORD = os.environ.get(password_at)
 
     if t:
         text = click.prompt('Text')
@@ -45,7 +51,7 @@ def send(to, user, t, text, f,  file, subject):
             with click.open_file(file,'rb') as file:
                 msg.add_attachment(file.read(), maintype='application', subtype='octet-stream', filename=file.name)
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    with smtplib.SMTP_SSL(*server) as smtp:
         try:
             smtp.login(user, EMAIL_PASSWORD)
         except smtplib.SMTPAuthenticationError:
@@ -63,12 +69,22 @@ def send(to, user, t, text, f,  file, subject):
 @email.command()
 @click.argument('alias')
 @click.argument('addr', nargs=-1)
-@click.option('-r', '--remove', is_flag=True)
+@click.option('-r', '--remove', is_flag=True, help='remove alias')
 def alias(alias, addr, remove):
+    """sets an alias to an address or a list of addresses"""
     if remove:
         config.remove_option('Aliases', alias)
     else:
         config.set('Aliases', alias, ' '.join(list(addr)))
+    with open('email_cli.cfg', 'w') as f:
+        config.write(f)
+
+
+@email.command('config')
+@click.argument('setting')
+@click.argument('value', nargs=-1)
+def config_(setting, value):
+    config.set('Settings', setting, ' '.join(value))
     with open('email_cli.cfg', 'w') as f:
         config.write(f)
 
